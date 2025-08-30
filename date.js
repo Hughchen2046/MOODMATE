13fc66020c342a95d3f90daa75eac46ee874539d
Hugh <hughchen2046@gmail.com>
2025-08-30 15:28:17 +0800

    fix index and record.html

--- date.js
+++ date.js
@@ -2,6 +2,9 @@
       const month = today.getMonth() + 1; // getMonth() 回傳 0-11
       const day = today.getDate();
       const weekday = today.toLocaleDateString('zh-TW', { weekday: 'long' });
+      const weekdayshort = today.toLocaleDateString('zh-TW', { weekday: 'short' }); 
       const dateStr = `${month}月${day}日 ${weekday}`;
+      const dateStr1 = `${day} ${weekdayshort}`;
 
       document.getElementById("date-todayheading").textContent = dateStr;
+         document.getElementById("date-todayheadingshort").textContent = dateStr1;   
