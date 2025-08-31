dfa918951ad03a6571a334fe6a609250ab627a5b
Hugh <hughchen2046@gmail.com>
2025-08-31 18:41:56 +0800

    update 快框

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+    // top的時間js
+    function updateTime() {
+        const now = new Date();
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        document.getElementById('time').textContent = `${hours}:${minutes}`;
+    }
+
+    updateTime();
+    setInterval(updateTime, 1000);
 
         // index->record 點擊 → 存選擇 → 換頁
         document.querySelectorAll('.mood-img').forEach(img => {
