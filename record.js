79fd9f11ce1d2348a514846907d9d4588dca9090
Hugh <hughchen2046@gmail.com>
2025-09-09 20:45:30 +0800

    fix js

--- record.js
+++ record.js
@@ -98,3 +98,14 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+      const today = new Date();
+      const month = today.getMonth() + 1; // getMonth() 回傳 0-11
+      const day = today.getDate();
+      const weekday = today.toLocaleDateString('zh-TW', { weekday: 'long' });
+      const weekdayshort = today.toLocaleDateString('zh-TW', { weekday: 'short' }); 
+      const dateStr = `${month}月${day}日 ${weekday}`;
+      const dateStr1 = `${day} ${weekdayshort}`;
+
+      document.getElementById("date-todayheading").textContent = dateStr;
+      document.getElementById("date-todayheadingshort").textContent = dateStr1;   
