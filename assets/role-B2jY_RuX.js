d3776e758464094059baead27658447513a0416f
Hugh <hughchen2046@gmail.com>
2025-09-11 00:07:20 +0800

    Updates

---
+++ assets/role-B2jY_RuX.js
@@ -0,0 +1,1 @@
+import"./main-DKyMz1tJ.js";import{i as t}from"./role-CAh8cVaj.js";document.getElementById("startBtn").addEventListener("click",()=>{t()});
--- assets/role-BEFwL0eX.js
+++
@@ -1,1 +0,0 @@
-import"./main-Dzr-k9KX.js";import{i as t}from"./role-CAh8cVaj.js";document.getElementById("startBtn").addEventListener("click",()=>{t()});
