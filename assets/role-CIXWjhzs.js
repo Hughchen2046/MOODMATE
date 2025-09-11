a88ce78714b3fe2867c2f35af8499f5a69ba2a4b
Rabbit <night31916@gmail.com>
2025-09-11 19:18:39 +0800

    Updates

---
+++ assets/role-CIXWjhzs.js
@@ -0,0 +1,1 @@
+import"./main-BI7dOTcB.js";import{i as t}from"./role-CAh8cVaj.js";document.getElementById("startBtn").addEventListener("click",()=>{t()});
--- assets/role-Z_0YkicZ.js
+++
@@ -1,1 +0,0 @@
-import"./main-CBG-OXDD.js";import{i as t}from"./role-CAh8cVaj.js";document.getElementById("startBtn").addEventListener("click",()=>{t()});
