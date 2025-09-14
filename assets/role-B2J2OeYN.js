1d1ed86d6af5bb77412eda97f21f8c2902d835f5
Rabbit <night31916@gmail.com>
2025-09-15 00:14:36 +0800

    Updates

--- assets/role-B2J2OeYN.js
+++
@@ -1,1 +0,0 @@
-import"./main-BnVdxUzb.js";import{i as t}from"./role-CAh8cVaj.js";document.getElementById("startBtn").addEventListener("click",()=>{t()});
---
+++ assets/role-C3JdMhzY.js
@@ -0,0 +1,1 @@
+import"./main-Q_t9GW8W.js";import{i as t}from"./role-CAh8cVaj.js";document.getElementById("startBtn").addEventListener("click",()=>{t()});
